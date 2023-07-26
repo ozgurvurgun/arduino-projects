194bf229a543bb9fac76c7521b703f9619b93646
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-07-26 17:41:25 +0300

    simple led button project added

---
+++ simple-led-button-circuit/simple-led-button-circuit.ino
@@ -0,0 +1,48 @@
+int i = 1;
+void setup() {
+  pinMode(12,INPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop() {
+if(digitalRead(12)){
+  i= i +1;
+  delay(250);
+}
+
+if(i == 1){
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+}
+
+if(i == 2){
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+}
+
+if(i == 3){
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+}
+
+else if(i == 4){
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+}
+if(i == 5)
+  i = 1;
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+}
